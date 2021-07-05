217a9e32e82ed4beecfddc975a28fb1a3d4be097
irhad kunovac <irhad_k@medit.ba>
2021-07-05 23:19:00 +0200

    about.hbs_changes

--- src/app.js
+++ src/app.js
@@ -41,7 +41,7 @@ app.get("/help/*", (req, res) =>{
 app.get("/about", (req, res) => {
     res.render("about", {
         title: "About page",
-        name: "Ihrad Kunovac"
+        name: "Irhad Kunovac"
     });
 });
 // app.get("*", (req, res) =>{
